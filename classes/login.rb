f22752f08c99baaa8835a41656eb5cc2a2063c59
root <apollo-3@mail.ru>
2016-03-22 15:09:25 +0000

    added functionality

--- classes/login.rb
+++ classes/login.rb
@@ -12,12 +12,20 @@ class Login < Grape::API
       end
     end
     post '/login' do
+      status 200
       client = Users.new
       return client.checkUser params[:user]
     end
 
-    get '/logout' do
-      return "{'success':'ok'}"
+    params do
+      requires :user, type: Hash do
+        requires :mail, type: String
+        requires :token, type: String
+      end
+    end    
+    post '/logout' do
+      client = Users.new
+      client.logout params[:user]
     end
   end
 end
--- classes/users.rb
+++ classes/users.rb
@@ -4,13 +4,17 @@ require_relative 'helper'
 
 class Users
   MSGS = {
-           'no_such_mail' => {'en' => 'There is no such email', 'ru' => 'Нет такой почты'},
-           'bad_user' => {'en' => 'User doesn\'t exists', 'ru' => 'Такой пользователь не существует'},
-           'bad_token' => {'en' => 'Token has expired', 'ru' => 'Токен недействителен'}
+            'no_such_mail' => {'en' => 'There is no such email', 'ru' => 'Нет такой почты'},
+            'bad_user' => {'en' => 'User doesn\'t exist', 'ru' => 'Такой пользователь не существует'},
+            'bad_token' => {'en' => 'Token has expired', 'ru' => 'Токен недействителен'},
+            'user_deleted' => {'en' => 'User was deleted', 'ru' => 'Пользователь был удален'},
+            'mail_exists' => {'en' => 'This email is already used', 'ru' => 'Данный адрес почты уже используется'},
+            'user_deleted' => {'en' => 'User was deleted', 'ru' => 'Пользователь удален'},
+            'user_updated' => {'en' => 'User was updated', 'ru' => 'Данные пользователя были изменены'}            
          }
   def initialize
     @db = Db.new 'apartments'
-  end
+  end  
   def setToken mail
     token = Digest::MD5.hexdigest "#{mail}.#{Time.now().to_i.to_s}" 
     @db.con[:users].find(:mail => mail).update_one({'$set' => {:token => token}})
@@ -40,5 +44,40 @@ class Users
         return {'error' => MSGS['no_such_mail']['en']}
     end
     @db.close
+  end
+  def logout user
+    valid_token = checkToken user['mail'], user['token']
+    if valid_token.has_key?('success')
+      setToken user[:mail]
+    end
+  end
+  def delUser user
+    valid_token = checkToken(user[:mail], user[:token])
+    if valid_token.has_key?('success')    
+      @db.con[:users].find(:mail => user[:mail]).delete_one
+      return {'success' => MSGS['user_deleted']['en']}
+    else
+      return valid_token
+    end
+    @db.close
+  end
+  def newUser user
+    obj = @db.con[:users].find(:mail => user[:mail])
+    if obj.count >= 1
+      return {'error' => MSGS['mail_exists']['en']}
+    else
+      @db.con[:users].insert_one user
+      return {'success' => MSGS['user_created']['en']}
+    end
+    @db.close
+  end
+  def updateUser old_mail, user
+    valid_token = checkToken(old_mail, user[:token])
+    if valid_token.has_key?('success')
+      @db.con[:users].find(user).update_one(user)
+      return {'success' => MSGS['user_updated']['en']}
+    else
+      return valid_token
+    end
   end
 end
