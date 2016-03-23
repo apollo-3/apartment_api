965c37812af5320c4a17e5bd8e94181cd579fe4f
root <apollo-3@mail.ru>
2016-03-23 15:33:55 +0000

    mastered signup part

--- classes/login.rb
+++ classes/login.rb
@@ -27,5 +27,26 @@ class Login < Grape::API
       client = Users.new
       client.logout params[:user]
     end
+
+    params do
+      requires :user, type: Hash do
+        requires :name, type: String
+        requires :mail, type: String
+        requires :password, type: String
+      end
+    end
+    post '/register' do
+      client = Users.new
+      return client.newUser params[:user]
+    end
+
+    params do
+      requires :mail, type: String
+      requires :token, type: String
+    end
+    get '/verify' do
+      client = Users.new
+      return client.setVerified params[:mail], params[:token]
+    end
   end
 end
--- classes/users.rb
+++ classes/users.rb
@@ -5,12 +5,14 @@ require_relative 'helper'
 class Users
   MSGS = {
             'no_such_mail' => {'en' => 'There is no such email', 'ru' => 'Нет такой почты'},
-            'bad_user' => {'en' => 'User doesn\'t exist', 'ru' => 'Такой пользователь не существует'},
+            'bad_pass' => {'en' => 'Password is wrong', 'ru' => 'Неверный пароль'},
             'bad_token' => {'en' => 'Token has expired', 'ru' => 'Токен недействителен'},
             'user_deleted' => {'en' => 'User was deleted', 'ru' => 'Пользователь был удален'},
             'mail_exists' => {'en' => 'This email is already used', 'ru' => 'Данный адрес почты уже используется'},
             'user_deleted' => {'en' => 'User was deleted', 'ru' => 'Пользователь удален'},
-            'user_updated' => {'en' => 'User was updated', 'ru' => 'Данные пользователя были изменены'}            
+            'user_updated' => {'en' => 'User was updated', 'ru' => 'Данные пользователя были изменены'},
+            'user_created' => {'en' => 'User was created', 'ru' => 'Пользователь был зарегистрирован'},
+            'not_verified' => {'en' => 'User is not verified', 'ru' => 'Пользователь не проверен'}
          }
   def initialize
     @db = Db.new 'apartments'
@@ -21,63 +23,87 @@ class Users
     return token
   end
   def checkToken mail, token
-    obj = @db.con[:users].find({:mail => mail, :token => token})    
+    resp = {'error' => MSGS['bad_token']['en']}
+    obj = @db.con[:users].find({:mail => mail, :token => token})
     if obj.count >= 1
       if obj.first[:token] == token
-        return {'success' => 'ok'}
-      else
-       return {'error' => MSGS['bad_token']['en']}
+        resp = {'success' => 'ok'}
       end
     end
+    return resp
   end
   def checkUser user
+    resp = nil
     obj = @db.con[:users].find(:mail => user[:mail])
     if obj.count >= 1 
       obj = obj.first
       if obj[:password] == user[:password]
-        token = setToken user[:mail]
-        return {'success' => 'ok', 'token' => token}
+        if obj[:verified]
+          token = setToken user[:mail]
+          resp = {'success' => 'ok', 'token' => token}
+        else
+          resp = {'error' => MSGS['not_verified']['en']}
+        end
       else
-        return {'error' => MSGS['bad_user']['en']}
+        resp = {'error' => MSGS['bad_pass']['en']}
       end
     else
-        return {'error' => MSGS['no_such_mail']['en']}
+        resp = {'error' => MSGS['no_such_mail']['en']}
     end
     @db.close
+    return resp
   end
   def logout user
     valid_token = checkToken user['mail'], user['token']
     if valid_token.has_key?('success')
       setToken user[:mail]
     end
+    @db.close
   end
   def delUser user
+    resp = nil
     valid_token = checkToken(user[:mail], user[:token])
     if valid_token.has_key?('success')    
       @db.con[:users].find(:mail => user[:mail]).delete_one
-      return {'success' => MSGS['user_deleted']['en']}
+      resp = {'success' => MSGS['user_deleted']['en']}
     else
-      return valid_token
+      resp = valid_token
     end
     @db.close
+    return resp
   end
   def newUser user
+    resp = nil
     obj = @db.con[:users].find(:mail => user[:mail])
     if obj.count >= 1
-      return {'error' => MSGS['mail_exists']['en']}
+      resp = {'error' => MSGS['mail_exists']['en']}
     else
-      @db.con[:users].insert_one user
-      return {'success' => MSGS['user_created']['en']}
+      user[:verified] = false
+      @db.con[:users].insert_one(user)
+      token = setToken user['mail']
+      resp = {'success' => MSGS['user_created']['en'], 'verifing_url' => "http://192.168.33.11/verify.html?mail=#{user[:mail]}&token=#{token}"}
     end
     @db.close
+    return resp
   end
   def updateUser old_mail, user
+    resp = nil
     valid_token = checkToken(old_mail, user[:token])
     if valid_token.has_key?('success')
-      @db.con[:users].find(user).update_one(user)
-      return {'success' => MSGS['user_updated']['en']}
+      @db.con[:users].find(user).update_one user
+      resp ={'success' => MSGS['user_updated']['en']}
     else
-      return valid_token
+      resp =  valid_token
     end
+    @db.close
+    return resp
+  end
+  def setVerified mail, token
+    valid_token = checkToken(mail, token)
+    if valid_token.has_key?('success')
+      @db.con[:users].find(:mail => mail).update_one({'$set' => {'verified' => true}})
+    end
+    @db.close
+    return valid_token
   end
 end
